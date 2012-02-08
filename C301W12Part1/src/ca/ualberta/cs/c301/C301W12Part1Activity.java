7605cecfce16505052d6b7108c1f757e2c3d4c00
fokaefs <fokaefs@ualberta.ca>
2012-02-07 17:30:49 -0700

    Initial Commit

---
+++ C301W12Part1/src/ca/ualberta/cs/c301/C301W12Part1Activity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.c301;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class C301W12Part1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
